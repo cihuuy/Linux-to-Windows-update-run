9e7704a33314e45be162c8d5b492750a631efe1f
cihuuy <95659029+cihuuy@users.noreply.github.com>
2024-03-18 01:02:45 +0700

    Update and rename Linux_QemuWindows7ColabNoVT-X.sh to Linux_RUN-QemuWindows7ColabNoVT-X.sh

--- Linux_RUN-QemuWindows7ColabNoVT-X.sh
+++ Linux_RUN-QemuWindows7ColabNoVT-X.sh
@@ -1,5 +1,4 @@
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz && tar -xf ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
 read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp 5900 &>/dev/null &
@@ -15,4 +14,4 @@ curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*publ
 echo "Note: Use Right-Click Or Ctrl+C To Copy"
 echo "Please Keep Colab Tab Open, Maximum Time 12h"
 echo Script by Noah Jami
-sudo qemu-system-x86_64 -vnc :0 -hda lite7.qcow2  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
+sudo qemu-system-x86_64 -vnc :0 -hda lite7.qcow2  -smp cores=3  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
